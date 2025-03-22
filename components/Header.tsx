0c8132cb1bf785e89a53da098bae8d8d38fa4c0e
Артём Оганесян <ar.oganesyan@atlassys.tech>
2025-03-22 19:17:55 +0300

    стилизация хедера

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,26 @@
+import { Bell, LogOut } from "lucide-react";
+
 export default function Header({ title }: { title: string }) {
   return (
-    <header className="flex justify-between items-center p-6 border-b bg-white">
-      <h1 className="text-2xl font-semibold">{title}</h1>
-      <div className="flex items-center space-x-3">
-        <span className="text-gray-700">Дмитрий</span>
-        <div className="w-8 h-8 rounded-full bg-pink-400 flex items-center justify-center text-white">
-          Д
+    <header
+      className="flex justify-between items-center p-6 bg-white"
+      style={{
+        boxShadow: "0 4px 10px rgba(0, 0, 0, 0.05)",
+      }}
+    >
+      <h1 className="text-[20px] text-gray-500  font-semibold">{title}</h1>
+      <div className="flex items-center space-x-6">
+        <div className="flex items-center space-x-3">
+          <div className="flex flex-col space-y-0.5">
+            <span className="text-gray-700 text-[16px]">Дмитрий</span>
+            <span className="text-gray-500 text-[12px]">Собственник</span>
+          </div>
+          <div className="w-10 h-10 rounded-full bg-pink-400 flex items-center justify-center text-white">
+            Д
+          </div>
         </div>
+        <Bell size={20} color="#8097B1" />
+        <LogOut size={20} color="#8097B1" />
       </div>
     </header>
   );
